7ab9e31d1e9a4de25190e6a52ae4e4f0a1f75161
CedricRgx <134865690+CedricRgx@users.noreply.github.com>
2023-08-27 17:45:37 +0200

    Étape 4 : Développez la fonctionnalité des 5 % de remise

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,14 +1,30 @@
 package com.parkit.parkingsystem.service;
 
 import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
     public void calculateFare(Ticket ticket) {
+
+	TicketDAO ticketDAO = new TicketDAO();
+
+	if (ticketDAO.getNbTicket(ticket) > 1) { // the vehicle has been already parked, then it has a 5% discount
+	    calculateFare(ticket, true);
+	} else {
+	    calculateFare(ticket, false);
+	}
+    }
+
+    public void calculateFare(Ticket ticket, boolean isDiscounted) {
 	if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
 	    throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
 	}
+
+	// Step 4: expand the 5% discount feature
+	double factorDiscount = isDiscounted ? 0.95 : 1; // if there is a 5% discount, the user has to pay 95% of the
+	// price
 
 	double inHour = ticket.getInTime().getTime();
 	double outHour = ticket.getOutTime().getTime();
@@ -22,11 +38,11 @@ public class FareCalculatorService {
 	} else {
 	    switch (ticket.getParkingSpot().getParkingType()) {
 	    case CAR: {
-		ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+		ticket.setPrice(factorDiscount * duration * Fare.CAR_RATE_PER_HOUR);
 		break;
 	    }
 	    case BIKE: {
-		ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+		ticket.setPrice(factorDiscount * duration * Fare.BIKE_RATE_PER_HOUR);
 		break;
 	    }
 	    default:
